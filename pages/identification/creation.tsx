86eb9290f338ee607869e9c6b5a618f97829ef5f
SuuSoJeat <suusojeat@gmail.com>
2022-02-18 15:43:38 +0700

    feat(creation): add identity creation page

--- pages/identification/creation.tsx
+++ pages/identification/creation.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { ReactElement } from "react";
 import IdentificationLayout from "../../components/identification-layout";
 
-export default function Verification() {
+export default function Creation() {
   return (
     <div className=" mx-auto flex flex-col items-center justify-center gap-8 rounded-md  p-5  backdrop-blur-md  dark:bg-white/5">
       <div className="flex w-full items-center justify-between">
         <h1 className=" select-none text-center font-sans text-2xl font-bold uppercase">Identity</h1>
-        <Link href="/identification/creation">
+        <Link href="/identification/verification">
           <a className="dark:bg-midnight dark:hover:border-crimson dark:hover:bg-crimson/50 focus-visible:outline-crimson select-none rounded-md p-2 px-4 font-semibold capitalize focus-visible:outline-none dark:hover:text-red-50">
-            Create
+            Verify
           </a>
         </Link>
       </div>
@@ -19,7 +19,6 @@ export default function Verification() {
           <input
             name="username"
             type="text"
-            autoComplete="username"
             required
             placeholder="Username"
             className="dark:bg-midnight focus:border-crimson caret-crimson block w-full rounded-md border-2 border-transparent placeholder:text-slate-500 focus:outline-none focus:ring-0 focus-visible:outline-none"
@@ -27,9 +26,15 @@ export default function Verification() {
           <input
             name="password"
             type="password"
-            autoComplete="current-password"
             required
             placeholder="Password"
+            className="dark:bg-midnight focus:border-crimson caret-crimson block w-full rounded-md border-2 border-transparent placeholder:text-slate-500 focus:outline-none focus:ring-0 focus-visible:outline-none "
+          />
+          <input
+            name="password-confirmation"
+            type="password"
+            required
+            placeholder="Password Confirmation"
             className="dark:bg-midnight focus:border-crimson caret-crimson block w-full rounded-md border-2 border-transparent placeholder:text-slate-500 focus:outline-none focus:ring-0 focus-visible:outline-none "
           />
         </div>
@@ -37,13 +42,13 @@ export default function Verification() {
           type="submit"
           className="dark:bg-midnight dark:hover:border-crimson dark:hover:bg-crimson/50 focus-visible:outline-crimson w-full select-none rounded-md p-2 px-4 font-semibold capitalize focus-visible:outline-none dark:hover:text-red-50"
         >
-          Verify
+          Create
         </button>
       </form>
     </div>
   );
 }
 
-Verification.getLayout = function (page: ReactElement) {
+Creation.getLayout = function (page: ReactElement) {
   return <IdentificationLayout>{page}</IdentificationLayout>;
 };
